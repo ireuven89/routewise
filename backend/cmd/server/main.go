b6580a48805c388d487a955cb1b6670f686eb8c8
ireuven89 <aitzik89@gmail.com>
2026-01-22 11:36:19 +0200

    remnoeed .env fle foro mgit

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -40,32 +40,6 @@ func main() {
 	router.Use(sentrygin.New(sentrygin.Options{}))
 
 	router.Use(middleware.Cors())
-	/*allowedOriginsStr := os.Getenv("ALLOWED_ORIGINS")
-	router.Use(func(c *gin.Context) {
-		origin := c.Request.Header.Get("Origin")
-		allowedOrigins := strings.Split(allowedOriginsStr, ",")
-
-		isAllowed := false
-		for _, allowedOrigin := range allowedOrigins {
-			if strings.TrimSpace(allowedOrigin) == origin {
-				isAllowed = true
-				break
-			}
-		}
-
-		if isAllowed {
-			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
-			c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, PATCH")
-			c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-			c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
-		}
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-		c.Next()
-	})*/
 
 	// Setup routes
 	api.SetupRoutes(router, db)
